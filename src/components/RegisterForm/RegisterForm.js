c71a39c4e0ed79bdc5ce2c2f7720dadc9534faa3
Stas-0k <brickbybrick15@gmail.com>
2023-03-12 00:26:02 +0200

    commit
    commit

---
+++ src/components/RegisterForm/RegisterForm.js
@@ -0,0 +1,26 @@
+export const RegisterForm = () => { 
+    return (
+        <div>
+      <h1>Welcome to registration form!</h1>
+
+      <form autoComplete="off">
+        <label>
+          Name
+          <input type="text" name="name"  />
+        </label>
+
+        <label>
+          E-mail
+          <input type="email" name="email"  />
+        </label>
+
+        <label>
+          Password
+          <input type="password" name="password" />
+        </label>
+
+        <button type="submit">Зарегистрироваться</button>
+      </form>
+    </div>
+    )
+}
